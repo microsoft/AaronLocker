cf3761a4d87740b0dc2d312bb85af693789ab36d
Ronny Moreas <ronny.moreas@kuleuven.be>
2019-04-08 18:25:19 +0200

    Use hashtable output in GetSafePathsToAllow.ps1 supporting "Label" and "RuleCollection" properties

--- AaronLocker/CustomizationInputs/GetSafePathsToAllow.ps1
+++ AaronLocker/CustomizationInputs/GetSafePathsToAllow.ps1
@@ -1,26 +1,60 @@
 <#
 .SYNOPSIS
-Customizable script used by Create-Policies.ps1 that produces a list of additional "safe" paths to allow for non-admin execution.
+Customizable script used by Create-Policies.ps1 that produces a list of "safe"
+paths to allow for non-admin execution.
 
 .DESCRIPTION
-This script outputs a simple list of directories that can be considered "safe" for non-admins to execute programs from.
-The list is consumed by Create-Policies.ps1, which incorporates the paths into AppLocker rules allowing execution of
-EXE, DLL, and Script files.
-NOTE: DIRECTORY/FILE PATHS IDENTIFIED IN THIS SCRIPT MUST NOT BE WRITABLE BY NON-ADMIN USERS!!!
+This script outputs zero or more hashtables containing information to define path rules
+for files of directories that can be considered "safe" for non-admins.
+
+The list is consumed by Create-Policies.ps1, which incorporates the paths
+into AppLocker rules allowing execution of Exe, Dll, and/or Script files.
+
+NOTE: DIRECTORY/FILE PATHS IDENTIFIED IN THIS SCRIPT SHOULD NOT BE WRITABLE
+BY NON-ADMIN USERS!!!
 You can edit this file as needed for your environment.
 
-Note that each directory name must be followed by \*, as in these examples:
-    "C:\ProgramData\App-V\*"
-    "\\MYSERVER\Apps\*"
-Individual files can be allowed by path, also. Do not end those with "\*"
+The hashtables need to have the following required properties: 
+* Label: String is incorporated into the rule name and description
+* Path: Path of file or directory to be whitelisted. Directories path always
+  need to end with "\*"", e.g.: "C:\ProgramData\App-V\*", "\\MYSERVER\Apps\*". Individual
+  files should not end with "\*".
 
-Specify paths using only fixed local drive letters or UNC paths. Do not use mapped drive letters or
-SUBST drive letters, as the user can change their definitions. If X: is mapped to the read-only
-\\MYSERVER\Apps file share, and you allow execution in \\MYSERVER\Apps\*, the user can run MyProgram.exe
-in that share whether it is referenced as \\MYSERVER\Apps\MyProgram.exe or as X:\MyProgram.exe. Similarly,
-AppLocker does the right thing with SUBSTed drive letters.
+The following properties are optional:
+* RuleCollection: to apply the trust only within a single RuleCollection.
+RuleCollection must be one of "Exe", "Dll", "Script", or "Msi", and it is CASE-SENSITIVE.
 
-TODO: At some point, reimplement with hashtable output supporting "label" and "RuleCollection" properties so that path rules have more descriptive names, and can be applied to specific rule collections>
+Specify paths using only fixed local drive letters or UNC paths. Do not use
+mapped drive letters or SUBST drive letters, as the user can change their
+definitions. If X: is mapped to the read-only \\MYSERVER\Apps file share,
+and you allow execution in \\MYSERVER\Apps\*, the user can run MyProgram.exe
+in that share whether it is referenced as \\MYSERVER\Apps\MyProgram.exe or
+as X:\MyProgram.exe. Similarly, AppLocker does the right thing with
+SUBSTed drive letters.
+
+Paths may contain all supported AppLocker path variables for well-known
+directories in Windows such as %WINDIR%, %SYSTEM32%, %OSDRIVE%, %PROGRAMFILES%,
+%REMOVABLE% and %HOT%.
+
+See also https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/applocker/understanding-the-path-rule-condition-in-applocker
+
+Besides the known AppLocker path variables, the following special path
+variables %USERPROFILE%, %LOCALAPPDATA% and %APPDATA% are also supported and
+will substituted to  "%OSDRIVE%\Users\*", "%OSDRIVE%\Users\*\AppData\Local"
+and "%OSDRIVE%\Users\*\AppData\Roaming" resp.
+
+Examples:
+
+@{
+    Label = 'Anaconda';
+    Path = '%LOCALAPPDATA%\Continuum\Anaconda3\PKGS\*';
+    RuleCollection = 'Exe'
+}
+
+@{
+    Label = 'App-V';
+    Path = '%OSDRIVE%\App-V\*';
+}
 
 #>
 
@@ -35,13 +69,26 @@ if ($null -ne $cs)
     if ($cs.PartOfDomain)
     {
         $computerDomain = $cs.Domain
-        "\\$computerDomain\netlogon\*"
-        "\\$computerDomain\sysvol\*"
+        @{
+            Label = "DC Shares";
+            Path = "\\$computerDomain\netlogon\*";
+        }
+        @{
+            Label = "DC Shares";
+            Path = "\\$computerDomain\sysvol\*";
+        }
+        
 		$userDomain = $env:USERDNSDOMAIN
 		if ($null -ne $userDomain -and $userDomain.ToUpper() -ne $computerDomain.ToUpper())
 		{
-        	"\\$userDomain\netlogon\*"
-        	"\\$userDomain\sysvol\*"
+            @{
+                Label = "DC Shares";
+                Path = "\\$userDomain\netlogon\*";
+            }
+            @{
+                Label = "DC Shares";
+                Path = "\\$userDomain\sysvol\*";
+            }    
 		}
     }
     else
@@ -51,7 +98,13 @@ if ($null -ne $cs)
 }
 
 ### Windows Defender put their binaries in ProgramData for a while. Comment this back out when they move it back.
-"%OSDRIVE%\PROGRAMDATA\MICROSOFT\WINDOWS DEFENDER\PLATFORM\*"
+@{
+    Label = "Windows Defender";
+    Path = "%OSDRIVE%\PROGRAMDATA\MICROSOFT\WINDOWS DEFENDER\PLATFORM\*";
+}    
 
-# Windows upgrade
-'C:\$WINDOWS.~BT\Sources\*'
+# Windows upgrade sources
+@{
+    Label = "Windows Upgrade";
+    Path = '%OSDRIVE%\$WINDOWS.~BT\Sources\*';
+}    
